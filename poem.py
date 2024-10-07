bff25587fc253e9d80e2447291fe028c2b41e44c
evan-101408 <evangonzalez232@gmail.com>
2024-10-07 13:00:45 +0000

    Added verse three

---
+++ poem.py
@@ -0,0 +1,9 @@
+print("""
+ Woke up this mornin', draggin' my feet, Thought about that turkey sandwich, oh so sweet. Packed it up with care, 
+tucked it in my bag, But now I'm sittin' here feelin' mighty sad.
+Oh, lunchbox blues, got me feelin' low, Hungry in the schoolyard, nowhere left to go. Left my heart and my PB&J at home, 
+Now I'm sittin' here, hungry and alone.
+My stomach's grumblin', clock's movin' slow, Friends all munchin' while I'm stuck in this woe. 
+Oh mama, wish you knew, the pain I'm feelin' here, That sandwich was my sunshine, now it's disappear.
+Oh, lunchbox blues, got me feelin' low, Hungry in the schoolyard, nowhere left to go. 
+Left my heart and my PB&J at home, Now I'm sittin' here, hungry and alone.""")
